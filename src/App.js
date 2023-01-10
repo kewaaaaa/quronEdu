2b161885b7e37d6a400e7fd875e36c6ae4aeda3a
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2023-01-10 15:43:38 +0500

    times

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./layout/navbar/Navbar";
 import HomePage from "./pages/home/HomePage";
 import QuranPageClass from "./pages/quran/QuranPageClass";
 import SinglePage from "./pages/singlePage/SinglePage";
+import Times from "./pages/times/Times";
 
 // http://api.alquran.cloud/v1/quran/ar.alafasy
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/quran" element={<QuranPageClass />} />
+          <Route path="/times" element={<Times />} />
           <Route path="/posts/:number" element={<SinglePage />} />
         </Routes>
       </div>
---
+++ src/pages/times/Times.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Laoding from '../../components/laoding/Laoding'
+
+const Times = () => {
+  return (
+    <div>
+        <Laoding />
+    </div>
+  )
+}
+
+export default Times
